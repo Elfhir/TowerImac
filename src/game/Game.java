11f74c335c993f3203ed7c5aac74088f4af53e6b
agreffard <aurelien.greffard@gmail.com>
2013-04-05 12:43:04 +0200

    Class Game (Skeleton)

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,11 +1,43 @@
 package game;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Game {
 	
-	// test
+	private ArrayList<Player> mPlayers;
+	private ArrayList<Base> mBases;
+	//private Map map;
+	//private LinkedList<Tower> mTowers;
+	private LinkedList<Agent> mAgents;
 	
-	public static void main(String[] args) {
-
+	
+	public Game() {
+		super();
+		mPlayers = new ArrayList<Player>();
+		mBases = new ArrayList<Base>();
+		mAgents = new LinkedList<Agent>();
+	}
+	
+	public Game(ArrayList<Player> mPlayers, ArrayList<Base> mBases, LinkedList<Agent> mAgents) {
+		super();
+		this.mPlayers = mPlayers;
+		this.mBases = mBases;
+		this.mAgents = mAgents;
 	}
 
+	// Constructor from a file
+	public Game(InputStream input) {
+		
+		// read xml file
+		//this(players, bases, agents);
+	}
+		
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
 }
