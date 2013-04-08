185fe148019031377f148afcde84bc577f0a5793
Elfhir <ta.thanhliem@gmail.com>
2013-04-09 00:01:05 +0200

    Create time package, and began Thread and Timer

---
+++ src/time/BaseThread.java
@@ -0,0 +1,23 @@
+package time;
+
+public class BaseThread implements Runnable{
+
+	public BaseThread() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/time/TimerGame.java
@@ -0,0 +1,24 @@
+package time;
+
+import javax.swing.Timer;
+
+public class TimerGame extends Timer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TimerGame() {
+		super(0, null);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
