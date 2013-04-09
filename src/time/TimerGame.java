7d19b3217acc0ead4655dbcbb6577f3e1bea4e71
Elfhir <ta.thanhliem@gmail.com>
2013-04-10 01:22:55 +0200

    Add better ctor ; now using Clock and Timerable =D

--- src/time/TimerGame.java
+++ src/time/TimerGame.java
@@ -4,33 +4,112 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class TimerGame extends JFrame
-{
+
+// Not sure if extending JFrame is useful, useless, or an error, or don't matter
+// Needed for setDefaultCloser, title and maybe ActionListener
+// See ctor line 82
+public class TimerGame extends JFrame {
 	private static final long serialVersionUID = 4728920536811198811L;
 
-
 	protected Timer timer;
+	private Clock clock;
 
-	public TimerGame() throws HeadlessException{
+	public Clock getClock() {
+		return clock;
+	}
+
+	public void setClock(Clock clock) {
+		this.clock = clock;
+	}
+
+	public Timer getTimer() {
+		return timer;
+	}
+
+	public void setTimer(Timer timer) {
+		this.timer = timer;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "TimerGame [timer=" + timer + ", clock=" + clock + "]";
+	}
+
+	// --------------------------------------------------------ctor---------------------
+	// default
+	public TimerGame(int delay) throws HeadlessException{
 		super();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle(":)");
+		setTitle("Pinage");
 
-		int delay = 1000; //milliseconds
-		ActionListener taskPerformer = new ActionListener() {
+		clock = new Clock(0,0,0);
+		
+		ActionListener task = new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
-				System.out.println(java.util.Calendar.getInstance().getTime().toString());		      
+				clock.timeChange();
+				System.out.println(clock);
 			}
 		};
 		setVisible(true);
-		timer = new Timer(delay, taskPerformer);
+		timer = new Timer(delay, task);
+		timer.start();
+
+	}
+	
+	// Clock initializing
+	public TimerGame(int delay, int s, int m, int h) throws HeadlessException{
+		super();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Pinage");
+
+		clock = new Clock(s,m,h);
+		
+		ActionListener task = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				clock.timeChange();
+				System.out.println(clock);	      
+			}
+		};
+		setVisible(true);
+		timer = new Timer(delay, task);
+		timer.start();
+
+	}
+	
+	// With a Timerable class implementing, for executing code from other classes, other stuff
+	public TimerGame(int delay, int s, int m, int h, final Timerable t) throws HeadlessException{
+		super();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Pinage");
+
+		clock = new Clock(s,m,h);
+		
+		ActionListener task = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				
+				// do whatever you want with a Class implementing Timerable
+				// in its compulsory void method runTimer()
+				
+				t.runTimer();   
+				
+				
+			}
+		};
+		// JFrame method ; then a timer is created, executing the task every delay milliseconds. At the instanciation timer
+		// is started. There is method for handle timer.
+		setVisible(true); 
+		timer = new Timer(delay, task);
 		timer.start();
 
 	}
 
 	public static void main(String[] args)
 	{	  
-		new TimerGame();
+		new TimerGame(1000);
 
 	}
 
