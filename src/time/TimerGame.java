e5265d4bc868c28dcb27cd5b1503c9e424cdde85
Elfhir <ta.thanhliem@gmail.com>
2013-04-10 00:28:44 +0200

    new TimerGame, it works but not using Timerable or Clock

--- src/time/TimerGame.java
+++ src/time/TimerGame.java
@@ -1,62 +1,37 @@
 package time;
-
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
-import javax.swing.Timer;
+public class TimerGame extends JFrame
+{
+	private static final long serialVersionUID = 4728920536811198811L;
 
-public class TimerGame{
-	
-	private Timer timer; 
-	private boolean flag; 
-	
-	public Timer getTimer() {
-		return timer;
-	}
 
-	public void setTimer(Timer timer) {
-		this.timer = timer;
-	}
+	protected Timer timer;
 
-	public boolean isFlag() {
-		return flag;
-	}
-
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
-	
-	//--------------------------------------ctor-------------------------
-	public TimerGame(int delay, final Timerable t) {
+	public TimerGame() throws HeadlessException{
 		super();
-		
-		// delay in milliseconds
-		timer = new Timer(delay, new ActionListener() {
-			
-		    @Override
-		    public void actionPerformed(ActionEvent ae) {
-		    	
-		        // An object implementing Timerable can execute runTimer() here.
-		        // runTimer() can execute whatever you want : display polygon,
-		    	// display time, boiled bacon ...
-		    	//
-		    	
-		    	t.runTimer();
-		    	
-		    }
-		    
-		});
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle(":)");
+
+		int delay = 1000; //milliseconds
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				System.out.println(java.util.Calendar.getInstance().getTime().toString());		      
+			}
+		};
+		setVisible(true);
+		timer = new Timer(delay, taskPerformer);
+		timer.start();
+
 	}
-	
-	public static void main (String[] args){
-		
-		
-		TimerGame clockOfGame = new TimerGame(1000, new Clock(0,0,0) );
-		
-		if(clockOfGame.isFlag() == true) clockOfGame.getTimer().start();
-		
-		
-		
+
+	public static void main(String[] args)
+	{	  
+		new TimerGame();
+
 	}
 
 }
