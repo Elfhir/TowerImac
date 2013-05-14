55e11043bbe1fb8584374376f9176a38df31163b
lotch <hochart.charlotte@hotmail.fr>
2013-05-14 12:18:52 +0200

    Add GroupAgent

---
+++ src/game/GroupAgent.java
@@ -0,0 +1,24 @@
+package game;
+
+//import javax.swing.JLabel;
+
+public class GroupAgent extends Agent{
+	private int NbAgents;
+	
+	public GroupAgent(){
+		
+	}
+	
+	public int getNb(){
+		return NbAgents;
+	}
+	
+	public int GroupSize(){
+		int Size;
+		Size = NbAgents/2;
+		return Size;		
+	}
+	
+	
+	
+}
