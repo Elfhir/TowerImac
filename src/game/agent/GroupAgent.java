030965708518d8614236320ce47432973ea9b168
Elfhir <ta.thanhliem@gmail.com>
2013-05-31 19:01:51 +0200

    Stop moving GroupAgent when arriving

--- src/game/agent/GroupAgent.java
+++ src/game/agent/GroupAgent.java
@@ -1,5 +1,6 @@
 package game.agent;
 
+import game.Game;
 import game.base.Base;
 
 import java.awt.Color;
@@ -12,18 +13,20 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.vecmath.Vector2f;
 
+import window.AppliWindow;
+
 //import javax.swing.JLabel;
 
 public class GroupAgent extends Agent {
-	
+
 	private int nbAgent;
 	private Base baseOrigin;
 	private Base baseDestination;
 	private Vector2f vectorDirector;
 	private BufferedImage image;
-	
+
 	public GroupAgent(int nbInitialAgent, Base source, Base destination){
-		
+
 		this.nbAgent = nbInitialAgent;
 		this.baseOrigin = source;
 		this.baseDestination = destination;
@@ -32,7 +35,7 @@ public class GroupAgent extends Agent {
 		this.computeVectorDirector();
 
 		this.setBackground(source.getPlayer().getColor());
-		
+
 		Dimension dim = new Dimension(this.nbAgent, this.nbAgent);
 		this.setSize(dim);
 		this.setPreferredSize(dim);
@@ -45,30 +48,53 @@ public class GroupAgent extends Agent {
 			e.printStackTrace();
 		}		
 	}
-	
+
+	/**
+	 * Compute the Vector director, i.e the good direction agent to go
+	 */
 	private void computeVectorDirector() {
 		this.vectorDirector = new Vector2f();
 		this.vectorDirector.x = this.baseDestination.getPosition().x-baseOrigin.getPosition().x;
 		this.vectorDirector.y = baseDestination.getPosition().y-baseOrigin.getPosition().y;
 		this.vectorDirector.normalize();
 	}
-	
+
 	public int getNbAgent() {
 		return this.nbAgent;
 	}
-	
+
 	/*public boolean AuthorizedMove(){
 		position = getPosition();
 		map[x][y] = -1;
 		if(position = map[x][y]) setMoving();
 		return true;
 	}*/
-	
+
+	/*
 	public void moveOneStep(){
 		float x = this.position.x + this.vectorDirector.x * this.speed;
 		float y = this.position.y += this.vectorDirector.y * this.speed;
 		this.setPosition(x, y);
 	}
-	
+	 */
+
+	public void moveOneStep(){
+
+		int radiusBase = baseDestination.getMight();
+		int distX  = 0, distY = 0;
+
+		distX = (int) Math.abs(this.position.getX() - baseDestination.getPosition().getX());
+		distY = (int) Math.abs(this.position.getY() - baseDestination.getPosition().getY());
+		
+		if(distX > radiusBase && distY > radiusBase) {
+			float x = this.position.x + this.vectorDirector.x * this.speed;
+			float y = this.position.y += this.vectorDirector.y * this.speed;
+			this.setPosition(x, y);
+		}
+		else{
+			return;
+		}
+	}
+
 }	
 
